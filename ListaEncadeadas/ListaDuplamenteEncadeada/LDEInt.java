59c68386c5e235f932981aae321a2de74c719ed2
nutelinha02 <arthcc@outlook.com>
2023-10-10 14:02:27 +0000

    terminada

--- ListaEncadeadas/ListaDuplamenteEncadeada/LDEInt.java
+++ ListaEncadeadas/ListaDuplamenteEncadeada/LDEInt.java
@@ -5,5 +5,30 @@ public class LDEInt {
     private LDENode ultimo;
     private int quant;
 
-    
+    public boolean isEmpty(){
+        return this.primeiro == null && this.ultimo == null && this.quant == 0;
+    }
+    public void inserirNoFinal(int valor){
+        LDENode novo,aux = this.primeiro;
+        novo = new LDENode(valor);
+        if(this.isEmpty()){
+            this.primeiro = novo;
+            this.ultimo = novo;
+            this.quant++;
+        }
+        else{
+            while(aux!=null){
+                if(aux.getProx()==null){
+                    novo.setAnt(aux);
+                    aux.setProx(novo);
+                    this.ultimo = novo;
+                    quant++;
+                }
+                else{
+                    aux = aux.getProx();
+                }
+            }
+        }
+
+    }
 }
